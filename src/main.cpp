a3867d1585b8a1b9cbffaab7705b1ae05473708b
Sabo345 <warsisabeel@gmail.com>
2026-05-20 19:28:54 +0530

    setup raylib and render first shape

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
+/*#include <iostream>
 
 int main(){
 
     std::cout<<"changing txt, success!\n";
     //git test...
+
+    //raylib test
+    std::cout<<"raylib-5.0 linked\n";
+
+}*/
+
+#include "raylib.h"
+
+int main(){
+    InitWindow(800, 600, "cppGame");
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        //DrawText("Let's go!", 300, 300, 20, BLACK);
+
+        DrawRectangle(75,75,100,100,{0,255,0,255});
+
+        DrawRectangle(50,50,100,100,{255,0,0,255});
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
